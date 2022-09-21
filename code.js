10dbf0f8ca5473d7e13832829c228517e711eec6
ehabfeki <ehab.shaaban@contentsquare.com>
2022-09-21 16:37:56 +0200

    feat: Revert to static metric values

--- code.js
+++ code.js
@@ -4,24 +4,24 @@ figma.ui.resize(480, 500)
 
 let tips = '';
 let metrics = {
-  TimeBeforeFirstTap: Math.random() * 2,
-  TapRate: Math.random() * 4,
-  NumberTap: Math.random() * 5,
-  AttractivenessRate: Math.random() * 3,
-  exposureRate: Math.random() * 2,
-  exposureTime: Math.random() * 5,
+  passRate: "70%",
+  conversionRate: "35%",
+  exposureRate: "99.99%",
+  exitRate: "15%",
+  timeBeforeFirstAction: "6s",
+  loadingTimeStandards: "2.5s",
 };
 let data = getDataHtml(metrics);
 let metadata = '';
 
 function getDataHtml(metrics) {
   return '<span class="metrics">' +
-              '<p class="metric">Time before first Tap: <span class="metric-value">'+metrics.TimeBeforeFirstTap+'s</span></p>' +
-              '<p class="metric">Tap Rate:   <span class="metric-value">'+metrics.TapRate+'%</span></p>' +
-              '<p class="metric">Number Tap:   <span class="metric-value">'+metrics.NumberTap+'</span></p>' +
-              '<p class="metric">Attractiveness Rate:   <span class="metric-value">'+metrics.AttractivenessRate+'%</span></p>' +
-              '<p class="metric">Exposure Rate:   <span class="metric-value">'+metrics.exposureRate+'%</span></p>' +
-              '<p class="metric">Exposure Time:   <span class="metric-value">'+metrics.exposureTime+'s</span></p>' +
+              '<p class="metric">Pass Rate: <span class="metric-value">'+metrics.passRate+'</span></p>' +
+              '<p class="metric">Conversion Rate:   <span class="metric-value">'+metrics.conversionRate+'</span></p>' +
+              '<p class="metric">Exposure Rate:   <span class="metric-value">'+metrics.exposureRate+'</span></p>' +
+              '<p class="metric">Exit Rate:   <span class="metric-value">'+metrics.exitRate+'</span></p>' +
+              '<p class="metric">Time Before First Action:   <span class="metric-value">'+metrics.timeBeforeFirstAction+'</span></p>' +
+              '<p class="metric">Loading Time Standards:   <span class="metric-value">'+metrics.loadingTimeStandards+'</span></p>' +
             '</span>'
 }
 
@@ -37,28 +37,28 @@ figma.on('selectionchange', () => {
           tips =
             '<h3>Tips</h3>'+
             '<p>1/ It\'s more likely if you change the font size and colors of the prices it will be more noticeable</p>'
-            '<p>2/ With our experience we can say <span class="metric-value">'+metrics.TapRate+'%</span> of users select this option first</p>';
+            '<p>2/ With our experience we can say <span class="metric-value">'+metrics.passRate+'%</span> of users select this option first</p>';
         }
         else if (selectionName =='First option delivery') {
           tips =
           '<h3>Tips</h3>'+
             '<p>1/ It\'s important to display this delivery option at the first place to encourage client to select it</p>'+
-            '<p>2/ With our experience we can say <span class="metric-value">'+metrics.TapRate+'%</span> of users select this option first</p>';
+            '<p>2/ With our experience we can say <span class="metric-value">'+metrics.passRate+'%</span> of users select this option first</p>';
         }
         else if (selectionName =='Second option delivery') {
           tips =
             '<h3>Tips/infos</h3>'+
-              '<p>2/ With our experience we can say the second option of delivery are selected by <span class="metric-value">'+metrics.TapRate+'%</span></p>'
+              '<p>2/ With our experience we can say the second option of delivery are selected by <span class="metric-value">'+metrics.passRate+'%</span></p>'
         }
         else if (selectionName =='Third option delivery') {
           tips =
             '<h3>Tips/infos</h3>'+
-            '<p>2/ With our experience we can say the third option of delivery are selected by <span class="metric-value">'+metrics.TapRate+'</span>%</p>'
+            '<p>2/ With our experience we can say the third option of delivery are selected by <span class="metric-value">'+metrics.passRate+'</span>%</p>'
         }
         else if (selectionName =='Recap column') {
           tips =
             '<h3>Tips/infos</h3>'+
-            '<p>2/ With our experience we can say the third option of delivery are selected by <span class="metric-value">'+metrics.TapRate+'</span>%</p>'
+            '<p>2/ With our experience we can say the third option of delivery are selected by <span class="metric-value">'+metrics.passRate+'</span>%</p>'
         }
         else {
           tips = ''
@@ -66,14 +66,7 @@ figma.on('selectionchange', () => {
 
         metadata = tips + data;
         sendMetadata(metadata)
-        // tips = ''
-        // data = ''
-        // metrics = ''
-        // metadata = ''
     }
-    // else {
-    //     console.log("You have to select a layer!")
-    // }
 })
 
 function sendMetadata(metadata) {
